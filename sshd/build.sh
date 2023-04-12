340909c6526516d1ee8e0576818d380e9c57fe27
dev <dev>
2023-04-12 17:22:20 +0300

    SSHD

--- sshd/build.sh
+++ sshd/build.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+source .env
+
 SSHD_VERSION=`cat ./app/version.txt`
 BUILD_NUMBER=`date +%s`
+
+IMAGE=${REPOSITORY}/sshd
+
+echo "Building ${IMAGE}, version=${SSHD_VERSION}, build_number=${BUILD_NUMBER}"
 docker build --no-cache \
     --build-arg BUILD_NUMBER=${BUILD_NUMBER} \
-    -t makitradigitalappsrepo.azurecr.io/sshd:latest \
-    -t makitradigitalappsrepo.azurecr.io/sshd:${SSHD_VERSION} \
+    -t ${IMAGE}:latest \
+    -t ${IMAGE}:${SSHD_VERSION} \
     . 
